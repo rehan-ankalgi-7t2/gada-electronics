9b136d73c506b10163e7497465cdd781d7f5e453
Rehan Ankalgi <rehanankalgi7t2@gmail.com>
2023-10-25 00:08:15 +0530

    LOGIN FEATURE - user api slice - auth slice - login functionality

--- frontend/src/screens/LoginScreen.js
+++ frontend/src/screens/LoginScreen.js
@@ -1,18 +1,48 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import { Button, InputAdornment, IconButton, TextField } from "@mui/material";
-import { Visibility, VisibilityOff } from "@mui/icons-material";
-import { Link } from "react-router-dom";
+import { Button, TextField } from "@mui/material";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { useLoginMutation } from "../slices/usersApiSlice";
+import { useDispatch, useSelector } from "react-redux";
+import Loader from "../components/Loader";
+import { setCredentials } from "../slices/authSlice";
+import { toast } from "react-toastify";
 
 const LoginScreen = () => {
-  const [showPassword, setShowPassword] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const [login, { isLoading }] = useLoginMutation();
+  const { search } = useLocation();
+  const sp = new URLSearchParams(search);
+  const redirect = sp.get("redirect") || "/";
+  const { userInfo } = useSelector((state) => state.auth);
 
-  const handleClickShowPassword = () => {
-    setShowPassword(!showPassword);
+  useEffect(() => {
+    if (userInfo) {
+      navigate(redirect);
+    }
+  }, [userInfo, redirect, navigate]);
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
   };
 
-  const handleMouseDownPassword = (e) => {
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleLoginSubmit = async (e) => {
     e.preventDefault();
+    try {
+      const response = await login({ email, password }).unwrap();
+      dispatch(setCredentials({ ...response }));
+      navigate(redirect);
+    } catch (error) {
+      // console.log(error);
+      toast.error(error?.data?.message || error?.error);
+    }
   };
 
   return (
@@ -28,6 +58,7 @@ const LoginScreen = () => {
         }}
       >
         <form
+          onSubmit={handleLoginSubmit}
           className="my-5"
           style={{
             width: "40%",
@@ -47,6 +78,8 @@ const LoginScreen = () => {
             fullWidth
             margin="normal"
             color="appleBlack"
+            onChange={handleEmailChange}
+            value={email}
           />
           <TextField
             label="Enter your password"
@@ -57,25 +90,20 @@ const LoginScreen = () => {
             margin="normal"
             color="appleBlack"
             variant="outlined"
-            endAdornment={
-              <InputAdornment position="end">
-                <IconButton
-                  aria-label="toggle password visibility"
-                  onClick={handleClickShowPassword}
-                  onMouseDown={handleMouseDownPassword}
-                  edge="end"
-                >
-                  {showPassword ? <VisibilityOff /> : <Visibility />}
-                </IconButton>
-              </InputAdornment>
-            }
+            onChange={handlePasswordChange}
+            value={password}
           />
-          <Button size="large" variant="contained" color="yellow">
+          <Button size="large" variant="contained" color="yellow" type="submit">
             Login
           </Button>
+
+          {isLoading && <Loader />}
         </form>
         <p>
-          Don't have an account? <Link to={"/register"}>Sign up here</Link>
+          Don't have an account?{" "}
+          <Link to={redirect ? `/register?redirect=${redirect}` : "/register"}>
+            Sign up here
+          </Link>
         </p>
       </Container>
     </div>
